731b09a7f3e82138f2a598806db1bcb416e8806b
Obez99 <giuliogx@live.it>
2022-01-10 15:14:45 +0100

    Adds migration file for trips table

---
+++ database/migrations/2022_01_10_140202_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 50);
+            $table->text("description");
+            $table->string("destinations", 100);
+            $table->smallInteger("price");
+            $table->string("image");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
