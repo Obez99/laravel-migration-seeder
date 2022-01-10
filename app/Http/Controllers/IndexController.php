d0400e513e7748ef6f65fa399dca2ebd27bad0a7
Obez99 <giuliogx@live.it>
2022-01-10 17:34:28 +0100

    Prints cards in iundex page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Trip;
+
 class IndexController extends Controller
 {
     function index()
     {
-        return view("index");
+        $trips = Trip::all();
+        return view("index", ["trips" => $trips]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts/bootstrap_default')
 
 @section('content')
-
+  <div class="container">
+    <div class="row row-cols-2">
+      @foreach ($trips as $trip)
+        <div class="col g-3">
+          <div class="card" style="width: 100%; height:100%">
+            <img src="{{$trip->image}}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-capitalize">{{$trip->name}}</h5>
+              <p class="card-text">{{$trip->description}}</p>
+              <a href="#" class="btn btn-primary">Più dettagli</a>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+  </div>
 @endsection
